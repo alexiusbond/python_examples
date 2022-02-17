cddd641a389c6485b0d6322c8548d7aeb58220df
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2022-02-17 11:07:39 +0600

    regex is added

---
+++ lesson_6_1.py
@@ -0,0 +1,32 @@
+import re
+
+text = 'AV Analytics Vidhya AV'
+
+result = re.match(r'AV', text)
+print(result)
+
+result = re.match(r'Analytics', text)
+print(result)
+
+result = re.search(r'Analytics', text)
+print(result)
+
+result = re.findall(r'AV', text)
+print(result)
+
+result = re.split(r' ', text, 1)
+print(result)
+
+result = re.sub(r' ', ' - ', text)
+print(result)
+
+pattern = re.compile('AV')
+result = pattern.findall(text)
+print(result)
+
+with open("test_regs.txt", "r", encoding="utf-8") as file:
+    content = file.read()
+    print(content)
+
+    print(re.findall(r'fruits\[\d\]', content))
+    print(re.findall(r'log\d*\.txt', content))
