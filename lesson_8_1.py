175e5248ad66706d0ebb8854420ec510c8bcecd7
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2022-02-20 03:10:37 +0600

    lesson 8 is added

--- lesson_8_1.py
+++ lesson_8_1.py
@@ -1,6 +1,4 @@
 import sqlite3
-from sqlite3 import Error
-
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -38,8 +36,8 @@ def create_student(conn, student):
     :param student:
     :return: student id
     """
-    sql = ''' INSERT INTO students(full_name, mark, age, hobby, birth_date)
-              VALUES(?,?,?,?,?); '''
+    sql = ''' INSERT INTO students(full_name, mark, hobby, birth_date, is_married)
+              VALUES(?, ?, ?, ?, ?); '''
     cur = conn.cursor()
     cur.execute(sql, student)
     conn.commit()
@@ -109,31 +107,39 @@ CREATE TABLE students (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 full_name VARCHAR (200) NOT NULL,
 mark DOUBLE (5, 2) NOT NULL DEFAULT 0.0,
-age INTEGER NOT NULL,
 hobby TEXT DEFAULT NULL,
-birth_date DATE NOT NULL
+birth_date DATE NOT NULL,
+is_married BOOLEAN DEFAULT FALSE
 ); '''
 
 conn = create_connection(database)
 
 # create tables
 if conn is not None:
-    # create projects table
-    # create_table(conn, sql_create_students_table)
     print("Connected successfully!")
 
-    # create_student(conn, ("Mark Daniels", 77.12, 22, "Football", "1999-01-02"))
-    # create_student(conn, ("Alex Brilliant", 77.12, 35, None, "1989-12-31"))
-    # create_student(conn, ("Diana Julls", 99.3, 15, "Tennis", "2005-01-22"))
-    # create_student(conn, ("Michael Corse", 100.0, 19, "Watching movies", "2001-01-12"))
+    create_table(conn, sql_create_students_table)
 
-    # update_student_mark(conn, (99.2, 12))
+    create_student(conn, ("Mark Daniels", 77.12, "Football", "1999-01-02", False))
+    create_student(conn, ("Alex Brilliant", 77.12, None, "1989-12-31", True))
+    create_student(conn, ("Diana Julls", 99.3, "Tennis", "2005-01-22", True))
+    create_student(conn, ("Michael Corse", 100.0, "Diving", "2001-09-17", True))
+    create_student(conn, ("Jack Moris", 50.2, "Fishing and cooking", "2001-07-12", True))
+    create_student(conn, ("Viola Manilson", 41.82, None, "1991-03-01", False))
+    create_student(conn, ("Joanna Moris", 100.0, "Painting and arts", "2004-04-13", False))
+    create_student(conn, ("Peter Parker", 32.0, "Travelling and bloging", "2002-11-28", False))
+    create_student(conn, ("Paula Parkerson", 77.09, None, "2001-11-28", True))
+    create_student(conn, ("George Newel", 93.0, "Photography", "1981-01-24", True))
+    create_student(conn, ("Miranda Alistoun", 87.55, "Playing computer games", "1997-12-22", False))
+    create_student(conn, ("Fiona Giordano", 66.12, "Driving fast", "1977-01-15", True))
 
-    # delete_student(conn, 1)
+    # update_student_mark(conn, (99.2, 2))
 
-    # select_all_students(conn)
+    # delete_student(conn, 4)
 
-    select_students_by_mark(conn, 90.0)
+    select_all_students(conn)
+
+    # select_students_by_mark(conn, 90.0)
     conn.close()
 else:
     print("Error! cannot create the database connection.")
